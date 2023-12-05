3728c18f001173d2983b2e914b3385447b2dec8b
Melk de Sousa <melk.sousap@gmail.com>
2023-12-05 11:19:31 -0300

    add env vars

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -1,11 +1,16 @@
-export const envs = {
-    aws: {
-        accessKeyId: '',
-        secretAccessKey: '',
-        region: ''
-    },
-    data: {
-        bucket: '',
-        key: '',
-    }
-}
+import { AWS_ACCESS_KEY_ID, AWS_DRIVER_LOAD, AWS_SECRET_ACCESS_KEY, AWS_VEHICLE_LOAD } from '@env';
+import { z } from 'zod';
+
+const EnvSchema = z.object({
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_DRIVER_LOAD: z.string().url(),
+  AWS_VEHICLE_LOAD: z.string().url(),
+});
+
+export const envs = EnvSchema.parse({
+  AWS_ACCESS_KEY_ID,
+  AWS_DRIVER_LOAD,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_VEHICLE_LOAD,
+});
---
+++ src/types/index.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const AWS_ACCESS_KEY_ID: string | undefined
+    export const AWS_SECRET_ACCESS_KEY: string | undefined
+    export const AWS_DRIVER_LOAD: string | undefined
+    export const AWS_VEHICLE_LOAD: string | undefined
+}
