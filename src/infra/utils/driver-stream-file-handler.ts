a1afa3347348a495099b81177cb1c25a8729426a
Melk de Sousa <melk.sousap@gmail.com>
2023-12-08 11:39:14 -0300

    remove background actions lib

--- src/infra/utils/driver-stream-file-handler.ts
+++ src/infra/utils/driver-stream-file-handler.ts
@@ -2,7 +2,6 @@ import { ReadStream, StreamFileHandler } from '@/core/drivers/download-drivers.u
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Alert, DeviceEventEmitter } from 'react-native';
-import BackgroundService from 'react-native-background-actions';
 import RNFetchBlob from 'react-native-blob-util';
 import { readString } from 'react-native-csv';
 import Realm from 'realm';
@@ -10,98 +9,93 @@ import { realmConnection } from '../realm/connection';
 import { DriverModel } from '../realm/models/Driver';
 
 export class DriverStreamFileHandler implements StreamFileHandler {
-  async execute(stream: ReadStream): Promise<void> {
-    await BackgroundService.start(
-      async () => {
-        const { size } = await RNFetchBlob.fs.stat(stream.path);
+  private handleData(
+    size: number,
+    bufferSize: number,
+    counterRows = 0,
+    buffer = '',
+    currentSizeRead = 0
+  ) {
+    return (chunk: string) => {
+      // append the buffer to the chunk
+      chunk = buffer + chunk;
+      buffer = '';
 
-        let buffer = '';
-        let counterRows = 0;
-        let currentSizeRead = 0;
+      const lastCaracter = chunk[chunk.length - 1];
+      // check if the chunk ends with a broken line
+      if (lastCaracter !== '\n') {
+        // find the last newline character
+        const lastNewlineIndex = chunk.lastIndexOf('\n');
+        if (lastNewlineIndex !== -1) {
+          // buffer the broken line
+          buffer = chunk.substring(lastNewlineIndex + 1);
+          // remove the broken line from the chunk
+          chunk = chunk.substring(0, lastNewlineIndex + 1);
+        } else {
+          // no newline character, buffer the entire chunk
+          buffer = chunk;
+          chunk = '';
+        }
+      }
 
-        stream.open();
-        const start = new Date();
+      const formattedChunk = readString(chunk)
+        .data.filter((row: string[]) => row.every((v) => !!v))
+        .map((row) => ({
+          status: row[0],
+          cnh: row[1],
+          name: row[2],
+          state: row[3],
+        }));
 
-        stream.onData((chunk) => {
-          // append the buffer to the chunk
-          chunk = buffer + chunk;
-          buffer = '';
-
-          const lastCaracter = chunk[chunk.length - 1];
-          // check if the chunk ends with a broken line
-          if (lastCaracter !== '\n') {
-            // find the last newline character
-            const lastNewlineIndex = chunk.lastIndexOf('\n');
-            if (lastNewlineIndex !== -1) {
-              // buffer the broken line
-              buffer = chunk.substring(lastNewlineIndex + 1);
-              // remove the broken line from the chunk
-              chunk = chunk.substring(0, lastNewlineIndex + 1);
-            } else {
-              // no newline character, buffer the entire chunk
-              buffer = chunk;
-              chunk = '';
-            }
-          }
-
-          const formattedChunk = readString(chunk)
-            .data.filter((row: string[]) => row.every((v) => !!v))
-            .map((row) => ({
-              status: row[0],
-              cnh: row[1],
-              name: row[2],
-              state: row[3],
-            }));
-
-          realmConnection.write(() => {
-            formattedChunk.forEach((driver) => {
-              realmConnection.create<DriverModel>(
-                'Driver',
-                {
-                  _id: new Realm.BSON.ObjectId(),
-                  name: driver.name,
-                  cnh: driver.cnh,
-                  state: driver.state,
-                },
-                Realm.UpdateMode.Modified
-              );
-            });
-          });
-
-          counterRows += formattedChunk.length;
-          currentSizeRead += stream.bufferSize;
-
-          const percentage = Math.floor((currentSizeRead / size) * 100);
-
-          DeviceEventEmitter.emit('read-file', percentage);
-        });
-
-        stream.onError(async (err) => {
-          console.log('oops', err);
-          DeviceEventEmitter.emit('running-task', true);
-        });
-
-        stream.onEnd(() => {
-          DeviceEventEmitter.emit('running-task', true);
-
-          Alert.alert(
-            'Atualização Finalizada!',
-            'Foram inseridas ' +
-              counterRows +
-              ' linhas em ' +
-              formatDistanceToNow(start, { includeSeconds: true, locale: ptBR })
+      realmConnection.write(() => {
+        formattedChunk.forEach((driver) => {
+          realmConnection.create<DriverModel>(
+            'Driver',
+            {
+              _id: new Realm.BSON.ObjectId(),
+              name: driver.name,
+              cnh: driver.cnh,
+              state: driver.state,
+            },
+            Realm.UpdateMode.Modified
           );
         });
-      },
-      {
-        taskName: 'drivers',
-        taskTitle: 'Atualização de Dados',
-        taskDesc: 'Atualização de Dados de Condutores',
-        taskIcon: {
-          name: 'ic_launcher',
-          type: 'mipmap',
-        },
-      }
-    );
+      });
+
+      counterRows += formattedChunk.length;
+      currentSizeRead += bufferSize;
+
+      const percentage = Math.floor((currentSizeRead / size) * 100);
+
+      DeviceEventEmitter.emit('read-file', percentage);
+    };
+  }
+
+  async execute(stream: ReadStream): Promise<void> {
+    const { size } = await RNFetchBlob.fs.stat(stream.path);
+
+    let counterRows = 0;
+
+    stream.open();
+    const start = new Date();
+
+    stream.onData(this.handleData(size, stream.bufferSize, counterRows));
+
+    stream.onError(async (err) => {
+      console.log('oops', err);
+      DeviceEventEmitter.emit('running-task', true);
+    });
+
+    stream.onEnd(() => {
+      DeviceEventEmitter.emit('running-task', true);
+
+      Alert.alert(
+        'Atualização Finalizada!',
+        'Foram inseridas ' +
+          counterRows +
+          ' linhas em ' +
+          formatDistanceToNow(start, { includeSeconds: true, locale: ptBR })
+      );
+    });
   }
 }
