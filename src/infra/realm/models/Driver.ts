410ba5f0f18be4f511dac15fd2251e92f308bf83
Melk de Sousa <melk.sousap@gmail.com>
2023-12-08 08:46:39 -0300

    add new properties

--- src/infra/realm/models/Driver.ts
+++ src/infra/realm/models/Driver.ts
@@ -3,6 +3,8 @@ import Realm from 'realm';
 export class DriverModel extends Realm.Object<DriverModel> {
   _id!: Realm.BSON.ObjectId;
   name!: string;
+  cnh!: string;
+  state!: string;
   timestamp: number = Math.round(new Date().getTime() / 1000);
 
   static schema: Realm.ObjectSchema = {
@@ -10,6 +12,8 @@ export class DriverModel extends Realm.Object<DriverModel> {
     properties: {
       _id: { type: 'objectId', indexed: true },
       name: { type: 'string' },
+      cnh: { type: 'string', indexed: 'full-text' },
+      state: { type: 'string' },
       timestamp: {
         type: 'int',
         default: () => Math.round(new Date().getTime() / 1000),
